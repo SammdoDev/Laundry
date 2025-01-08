931d0336ff279f28a1e40aecd86ea5218e277663
samdo817 <phantom72008@gmail.com>
2025-01-08 13:21:24 +0700

    membuat landing page yg akan menuju ke beranda laundry

--- app/src/main/java/com/laundry/landing_page.kt
+++ app/src/main/java/com/laundry/landing_page.kt
@@ -1,20 +1,23 @@
 package com.laundry
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Landing_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.landing_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val nextButton: CardView = findViewById(R.id.next)
+
+        nextButton.setOnClickListener{
+            val intent = Intent(this, Beranda_laundry::class.java)
+            startActivity(intent)
         }
+
     }
 }
