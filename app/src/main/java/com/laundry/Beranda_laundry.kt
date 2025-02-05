83a982b3237172e2f69b2bf32ac50409c5fccb94
samdo817 <phantom72008@gmail.com>
2025-02-05 10:39:57 +0700

    membuat package pegawai

--- app/src/main/java/com/laundry/Beranda_laundry.kt
+++ app/src/main/java/com/laundry/Beranda_laundry.kt
@@ -1,14 +1,15 @@
 package com.laundry
 
+import android.content.Intent
 import android.icu.util.Calendar
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.text.SimpleDateFormat
-import java.util.Locale
+import pelanggan.data_pelanggan
 
 class Beranda_laundry : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,27 +17,34 @@ class Beranda_laundry : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.beranda_laundry)
 
-        val headline: TextView = findViewById(R.id.headline)
+        // Inisialisasi TextView untuk headline
+        val headline = findViewById<TextView>(R.id.headline)
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
 
-        val calender = Calendar.getInstance()
-        val hour = calender.get(Calendar.HOUR_OF_DAY)
-
-        val greeting = when{
-            hour in 0..11 -> "Selamat Pagi, Samuel"
-            hour in 12..17 -> "Selamat Siang, Samuel"
+        val greeting = when (hour) {
+            in 0..11 -> "Selamat Pagi, Samuel"
+            in 12..17 -> "Selamat Siang, Samuel"
             else -> "Selamat Malam, Samuel"
         }
-
         headline.text = greeting
 
-        val timeTextView: TextView = findViewById(R.id.time)
-
-        val sdf = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
-        val currentDate = sdf.format(calender.time)
-
+        // Inisialisasi TextView untuk waktu
+        val timeTextView = findViewById<TextView>(R.id.time)
+        val sdf = java.text.SimpleDateFormat("dd MMMM yyyy", java.util.Locale.getDefault())
+        val currentDate = sdf.format(calendar.time)
         timeTextView.text = currentDate
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        // Inisialisasi FloatingActionButton untuk navigasi ke data_pelanggan
+        val cardTambah: CardView = findViewById(R.id.card_tambah)
+        cardTambah.setOnClickListener {
+            val intent = Intent(this, data_pelanggan::class.java)
+            startActivity(intent)
+        }
+
+        // Penyesuaian padding untuk insets sistem
+        val mainLayout = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.main)
+        ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
